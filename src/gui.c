b5d4a1934af79904723bcb683afc7165c337a3ac
JGomezC145 <jgomezcyn@outlook.com>
2025-06-27 20:30:58 -0600

    Testing keysim with commands

--- src/gui.c
+++ src/gui.c
@@ -2,24 +2,52 @@
 #include "serial.h"
 #include <gtk/gtk.h>
 #include <string.h>
+#include <linux/input-event-codes.h>
+#include "keysim.h"
 
 static GtkLabel *status_label;
 
 static gboolean poll_serial(gpointer user_data) {
     char buffer[128];
     int n = serial_read_line(buffer, sizeof(buffer));
+    // Limpiar '\r' si existe
+    char *p = strchr(buffer, '\r');
+    if (p) *p = '\0';
+
+
+
     if (n > 0) {
-        printf("Recibido: >>>%s<<<\n", buffer);  // Para depurar en terminal
+        //printf("Recibido: >>>%s<<<\n", buffer);  // Para depurar en terminal
 
         if (strncmp(buffer, "Btn", 3) == 0 || strncmp(buffer, "Encoder", 7) == 0) {
-            gtk_label_set_text(status_label, buffer);  // Mostrar directamente el comando recibido
+            gtk_label_set_text(status_label, buffer);
         }
+
+        if (strcmp(buffer, "Btn1") == 0) {
+            gtk_label_set_text(status_label, "Botón 1");
+            keysim_send(KEY_B);
+        } else if (strcmp(buffer, "Btn2") == 0) {
+            gtk_label_set_text(status_label, "Botón 2");
+            keysim_send(KEY_ENTER);
+        } else if (strcmp(buffer, "EncoderL") == 0) {
+            gtk_label_set_text(status_label, "Encoder izquierda");
+            keysim_send(KEY_VOLUMEDOWN);
+        } else if (strcmp(buffer, "EncoderR") == 0) {
+            gtk_label_set_text(status_label, "Encoder derecha");
+            keysim_send(KEY_VOLUMEUP);
+        } else if (strcmp(buffer, "EncoderSW") == 0) {
+            gtk_label_set_text(status_label, "Encoder presionado");
+            keysim_send(KEY_PLAYPAUSE);
+        }
+
     }
     return TRUE;
 }
 
 // Crea una funcion para cerrar la app
 static void close_app(GtkWidget *widget, gpointer data) {
+    serial_close();
+    keysim_close();
     serial_close();
     gtk_main_quit();
 }
@@ -51,9 +79,15 @@ int gui_start(int argc, char *argv[], const char *serialPort) {
         g_timeout_add(100, poll_serial, NULL);
     }
 
+    if (keysim_init() != 0) {
+        gtk_label_set_text(status_label, "Error con uinput");
+    }
+
     gtk_widget_show_all(window);
     gtk_main();
 
+    serial_close();
+    keysim_close();
     serial_close();
     return 0;
 }
